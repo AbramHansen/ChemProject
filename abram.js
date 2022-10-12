03175d1a72e2fe260235140a048fadd8398db03e
abram_hansen <hansenabram@gmail.com>
2022-10-12 14:09:48 -0600

    prototype of atoms

--- abram.js
+++ abram.js
@@ -1,18 +1,71 @@
-var c = document.getElementById("testCanvas");
-var ctx = c.getContext("2d");
-for (let i = 0; i < 10; i++) {
-    var len = Math.floor(Math.random() * 300);;
-    var degree = Math.random() * 6.28318;
-    var x = Math.floor(Math.random() * 800);;
-    var y = Math.floor(Math.random() * 800);;
+const canvas = document.getElementById("testCanvas");
+const ctx = canvas.getContext("2d");
 
-    for (let j = 0; j < 100; j++) {
-        ctx.moveTo(x,y);
-        x += Math.cos(degree) * len;
-        y += Math.sin(degree) * len;
-        ctx.lineTo(x,y);
-        degree += 30;
-        len /= 1.1;
-        ctx.stroke();
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+class Electron {
+    constructor(shell) {
+        this.shell = shell;
     }
 }
+
+class Atom {
+    constructor(type, x, y) {
+        this.type = type;
+        this.x = x;
+        this.y = y;
+        this.angle = 0
+        if(type == 11) {
+            this.protons = 11;
+            this.neutrons = 11;
+            this.electrons = [2,8,1]; //number of electrons in each shell
+        }
+    }
+
+    draw(ctx) {
+        ctx.beginPath();
+
+        //draw nucleus
+        ctx.moveTo(this.x, this.y);
+        ctx.fillStyle = '#FF0000';
+        ctx.strokeStyle = "black";
+        ctx.arc(this.x, this.y, 10, 0, 2 * Math.PI, false);
+        ctx.fillStyle = '#FF0000';
+        ctx.fill();
+        ctx.stroke();
+
+        //draw electrons
+        var distanceFromNucleus = 20;
+        var layer = 1;
+        for (var shell of this.electrons) {
+            for (let i = 0; i < shell; i++) {
+                const speed = 2;
+                var electronAngle = this.angle * layer * speed + Math.PI * 2 / shell * i;
+                var electronX = this.x + Math.cos(electronAngle) * distanceFromNucleus;
+                var electronY = this.y + Math.sin(electronAngle) * distanceFromNucleus;
+
+                ctx.moveTo(electronX,electronY);
+                ctx.arc(electronX, electronY, 5, 0, 2 * Math.PI, false);
+                ctx.fillStyle = '#03e8fc';
+                ctx.fill();
+            }
+            distanceFromNucleus += 20;
+            layer++;
+        }
+        this.angle += 0.02;
+        this.x += 0.1;
+        this.y += 0.1;
+    }
+}
+
+sodiumAtom = new Atom(11, 100, 100);
+
+function animate() {
+    ctx.clearRect(0,0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+    sodiumAtom.draw(ctx);
+    requestAnimationFrame(animate);
+}
+
+animate()
