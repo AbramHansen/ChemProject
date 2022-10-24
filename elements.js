07c0e907d60431217b4d22542e49246890f1c720
katelyndb <97479562+katelyndb@users.noreply.github.com>
2022-10-24 15:00:51 -0600

    Working on things

--- elements.js
+++ elements.js
@@ -1,3 +1,5 @@
+const box = document.querySelector('#elementInfo');
+
 
 function changetext(){
     element1.innerHTML = 'newtext'
@@ -6,6 +8,50 @@ function changetext(){
 const element1 = document.querySelector("#element1")
 element1.addEventListener('click', (event => {
     changetext()}))
+
+
+
+
+    function getJSON() {
+        fetch("./elements.json")
+          .then((res) => {
+            return res.json();
+          })
+          .then((data) => console.log(data));
+        }
+
+let elementslist = getJSON();
+getJSON().elements.forEach(element => {
+    let section = document.createElement('section');
+    let name = document.createElement('p');
+    let number = document.createElement('h3');
+    let symbol = document.createElement('h3');
+    let image = document.createElement('img');
+    name.innerHTML = `${element.name}`;
+    number.innerHTML = `${element.number}`;
+    image.setAttribute('src', element.image.url);
+    image.setAttribute('alt', `${element.image.title}`);
+    symbol.innerHTML = `${element.symbol}`;
+    section.append(name);
+    section.append(number);
+    section.append(image);
+    section.append(symbol);
+    box.appendChild(section);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
