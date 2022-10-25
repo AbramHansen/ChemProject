f78b935f48a92c8452214f3fe758935f6b4ebc09
AdvancedEel850 <90942089+AdvancedEel850@users.noreply.github.com>
2022-10-25 14:22:06 -0600

    updated elements

--- js/elements.js
+++ js/elements.js
@@ -1,13 +1,7 @@
 const box = document.querySelector('#elementInfo');
 
+const elementbox = document.querySelector("#Element")
 
-function changetext(){
-    element1.innerHTML = 'newtext'
-}
-
-const element1 = document.querySelector("#element1")
-element1.addEventListener('click', (event => {
-    changetext()}))
 
 
 
@@ -15,37 +9,71 @@ element1.addEventListener('click', (event => {
 
 
 // Fetches the Elements.json file
-function getJSON() {
-    fetch("./elements.json")
-    .then(response => response.json())
-    .then(json => console.log(json));
-}
-// Returns the JSON File
-let elementslist = getJSON();
-getJSON().elements.forEach(element => {
-    let section = document.createElement('section');
-    let name = document.createElement('p');
-    let number = document.createElement('h3');
-    let symbol = document.createElement('h3');
-    let image = document.createElement('img');
-    name.innerHTML = `${element.name}`;
-    number.innerHTML = `${element.number}`;
-    image.setAttribute('src', element.image.url);
-    image.setAttribute('alt', `${element.image.title}`);
-    symbol.innerHTML = `${element.symbol}`;
-    section.append(name);
-    section.append(number);
-    section.append(image);
-    section.append(symbol);
-    box.appendChild(section);
-})
+// function getJSON() {
+//     fetch("./elements.json")
+//     .then(response => response.json())
+//     .then(json => console.log(json));
+// }
+// // Returns the JSON File
+// let elementslist = getJSON();
+// getJSON().elements.forEach(element => {
+//     let section = document.createElement('section');
+//     let name = document.createElement('p');
+//     let number = document.createElement('h3');
+//     let symbol = document.createElement('h3');
+//     let image = document.createElement('img');
+//     name.innerHTML = `${element.name}`;
+//     number.innerHTML = `${element.number}`;
+//     image.setAttribute('src', element.image.url);
+//     image.setAttribute('alt', `${element.image.title}`);
+//     symbol.innerHTML = `${element.symbol}`;
+//     section.append(name);
+//     section.append(number);
+//     section.append(image);
+//     section.append(symbol);
+//     box.appendChild(section);
+// })
+
+
+
+//will finish later
+
+async function loadElements() {
+    let elements = await getElements();
+    appendElements(elements);
+  }
+  
+  async function getElements() {
+    let response = await fetch("js/elements.json");
+  
+    if (response.ok) {
+      data = await response.json();
+      data = data.elements;
+      console.log(data);
+      return data;
+    } else {
+      throw Error(await response.text());
+    }
+  }
+  
+  function appendElements(data) {
+    let elementsContainer = document.getElementById("elements");
+  
+    data.forEach((elements) => {
+      let element = document.createElement("p");
+      element.textContent = elements.name;
+      element.classList.add("Element");
+    //   element.addEventListener("click", findElement);
+  
+      elementsContainer.appendChild(element);
+    });
+  }
 
 
 
 
 
-
-
+loadElements()
 
 
 
