75adc63d571e8e9e66d8f52fc328b36d16b1d7f3
ryanmadsenn <97546348+ryanmadsenn@users.noreply.github.com>
2022-11-28 15:55:48 -0700

    Removed a console.log

--- js/script.js
+++ js/script.js
@@ -1,18 +1,6 @@
 document.getElementById("switch").addEventListener("change", switchEvent);
 
 function switchEvent(e) {
-  //   let h2s = document.getElementsByTagName("h2");
-
-  //   for (let i = 0; i < h2s.length; i++) {
-  //     h2s[i].style.fontFamily = e.target.checked ? "minecraft-heading" : "Arial";
-  //   }
-
-  //   let h3s = document.getElementsByTagName("h3");
-
-  //   for (let i = 0; i < h3s.length; i++) {
-  //     h3s[i].style.fontFamily = e.target.checked ? "minecraft-heading" : "Arial";
-  //   }
-
   console.log(document.styleSheets);
 
   for (let i = 0; i < document.styleSheets[0].cssRules.length; i++) {
@@ -21,7 +9,6 @@ function switchEvent(e) {
       document.styleSheets[0].cssRules[i].selectorText ==
       "h1, h2, h3, h4, h5, h6"
     ) {
-      console.log(document.styleSheets[0].cssRules[i]);
       document.styleSheets[0].cssRules[i].style.fontFamily = e.target.checked
         ? "minecraft-heading"
         : "Arial";
