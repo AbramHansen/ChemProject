b284ddc7a38e42cca532857dc02c5a036a2d498c
ryanmadsenn <97546348+ryanmadsenn@users.noreply.github.com>
2022-11-28 15:50:29 -0700

    Fixed error with switch

--- js/script.js
+++ js/script.js
@@ -1,16 +1,31 @@
 document.getElementById("switch").addEventListener("change", switchEvent);
 
 function switchEvent(e) {
-  let h2s = document.getElementsByTagName("h2");
+  //   let h2s = document.getElementsByTagName("h2");
 
-  for (let i = 0; i < h2s.length; i++) {
-    h2s[i].style.fontFamily = e.target.checked ? "minecraft-heading" : "Arial";
-  }
+  //   for (let i = 0; i < h2s.length; i++) {
+  //     h2s[i].style.fontFamily = e.target.checked ? "minecraft-heading" : "Arial";
+  //   }
 
-  let h3s = document.getElementsByTagName("h3");
+  //   let h3s = document.getElementsByTagName("h3");
 
-  for (let i = 0; i < h3s.length; i++) {
-    h3s[i].style.fontFamily = e.target.checked ? "minecraft-heading" : "Arial";
+  //   for (let i = 0; i < h3s.length; i++) {
+  //     h3s[i].style.fontFamily = e.target.checked ? "minecraft-heading" : "Arial";
+  //   }
+
+  console.log(document.styleSheets);
+
+  for (let i = 0; i < document.styleSheets[0].cssRules.length; i++) {
+    // console.log(document.styleSheets[0].cssRules[i].);
+    if (
+      document.styleSheets[0].cssRules[i].selectorText ==
+      "h1, h2, h3, h4, h5, h6"
+    ) {
+      console.log(document.styleSheets[0].cssRules[i]);
+      document.styleSheets[0].cssRules[i].style.fontFamily = e.target.checked
+        ? "minecraft-heading"
+        : "Arial";
+    }
   }
 
   document.body.style.fontFamily = e.target.checked
