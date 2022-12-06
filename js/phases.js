54e0121eba65823a42046cbfc430e3713b5e0c0e
Ben Painter <ben4painter@gmail.com>
2022-12-06 09:14:06 -0700

    minecraft pictures

---
+++ js/phases.js
@@ -0,0 +1,25 @@
+document.getElementById("switch").addEventListener("change", switchEvent);
+function switchEvent(e) {
+
+    let solidImg = document.getElementById("solid")
+    let liquidImg = document.getElementById("liquid")
+    let gasImg = document.getElementById("gas")
+    let plasmaImg = document.getElementById("plasma")
+
+    solidImg.src = e.target.checked
+    ? "images\\phases\\MinecraftIce.jpg"
+    : "images\\phases\\Ice.jpg";
+
+    liquidImg.src = e.target.checked
+    ? "images\\phases\\MinecraftWater.jpg"
+    : "images\\phases\\water.jpg";
+
+    gasImg.src = e.target.checked
+    ? "images\\phases\\MinecraftSteam.jpg"
+    : "images\\phases\\steam.jpg";
+
+    plasmaImg.src = e.target.checked
+    ? "images\\phases\\minecraftSun.jpg"
+    : "images\\phases\\sun.jpg";
+
+}
